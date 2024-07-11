4709c8940aaf7ff2d72acd72f5850467afbd6679
isacborgert2024 <isacborgert@gmail.com>
2024-07-11 12:41:36 -0300

    Add files via upload

---
+++ backand-change-password-user-radius.php
@@ -0,0 +1,77 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+//session_start();
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+
+$dbcon = mysqli_connect('localhost', 'radius', 'G@s%w&rJ', 'radius');
+
+
+$curPassword  = $_POST['curPassword'];
+$newPassword = $_POST['newPassword'];
+$confirmPass = $_POST['confirmPassword'];
+$username = preg_replace("/[^A-Za-z0-9. ]/", "", $_POST['username']);
+
+$find_letters = array('{', '}', '[', ']', '*', '%','"',"'");
+$msg = '';
+
+try {
+        if (strposa($curPassword, $find_letters) == true || strposa($newPassword, $find_letters) == true || strposa($confirmPass, $find_letters) == true)
+                throw new Exception("Sua senha não pode conter aspas simples, duplas, *, %, [], {}");
+
+        if (!senhaValida($newPassword))
+                throw new Exception($msg = "Senha muito fraca");
+
+        $dbPassword = mysqli_query($dbcon, "select username,value from radcheck where username = '$username'");
+        $dbPassword = mysqli_fetch_assoc($dbPassword);
+        $dbusername = $dbPassword['username'];
+        $dbPassword = $dbPassword['value'];
+        $curPassword = crypt($curPassword, 'SALT_DALORADIUS');
+
+        $newPass = crypt($newPassword, 'SALT_DALORADIUS');
+
+        if (empty($dbusername))
+                throw new Exception("User does not exist");
+
+        if ($newPassword <> $confirmPass)
+                throw new Exception("Your passwords do not match");
+
+        if($curPassword != $dbPassword)
+                throw new Exception("Wrong current password");
+
+        if (mysqli_query($dbcon, "UPDATE radcheck SET value='$newPass' WHERE username='$username'"))
+                throw new Exception("You have successfully changed your password.");
+
+        if (mysqli_error($dbcon))
+                throw new Exception($dbcon);
+
+
+        mysqli_close($dbcon);
+
+}
+catch (Exception $e) {
+        $msg = $e->getMessage();
+        header("location:change-password-user-radius.php?msg=$msg");
+}
+
+function strposa($haystack, $needles=array(), $offset=0) {
+        $chr = array();
+        foreach($needles as $needle) {
+                $res = strpos($haystack, $needle, $offset);
+                if ($res !== false) $chr[$needle] = $res;
+        }
+        if(empty($chr)) return false;
+        return min($chr);
+}
+function senhaValida($senha) {
+    return preg_match('/[a-z]/', $senha) // tem pelo menos uma letra minúscula
+     && preg_match('/[A-Z]/', $senha) // tem pelo menos uma letra maiúscula
+     && preg_match('/[0-9]/', $senha) // tem pelo menos um número
+     && preg_match('/^[\w$@]{6,}$/', $senha); // tem 6 ou mais caracteres
+}
+?>
+
+
+
---
+++ change-password-user-radius.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8"/>
+<title>Change Password Radius</title>
+</head>
+<body>
+<b><font color="blue" size=5>
+Senhas apenas no formato Crypt-Password, mínimo 6 caracteres com letra maiúscula, minúscula e numero.
+<b></font size=5>
+    <form name="frmChange" role="form" class="form-signin" method="POST" action="backand-change-password-user-radius.php">
+
+  <div class="form-group">
+    <label for="InputUser">User Name</label>
+    <input type="text" class="form-control" id="InputUser" placeholder="User Name" name="username">
+    <label for="InputPassword1">Current Password</label>
+    <input type="password" class="form-control" id="InputPassword1" placeholder="Current Password" name="curPassword">
+    <label for="InputPassword2">New Password</label>
+    <input type="password" class="form-control" id="InputPassword2" placeholder="New Password" name="newPassword">
+     <label for="InputPassword3">Confirm New Password</label>
+    <input type="password" class="form-control" id="InputPassword3" placeholder="Confirm Password" name="confirmPassword">  </div>
+  <button class="btn btn-lrg btn-default btn-block" type="submit" value="send">Change it</button>
+
+
+      </div>
+
+
+      </form>
+
+</body>
+</html>
+<?php
+if( isset($_GET['msg']) ) {
+        echo '<script type="text/javascript">alert("' .$_GET['msg'] . '");</script>';
+}
+?>
+
+
+
+
